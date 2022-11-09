5c9060a4a4c5e6ee1b75cd210bae54872ce67f55
eliashead <eliashead.costa@gmail.com>
2022-11-09 13:42:24 -0300

    feat: add component Heading

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+
+  args: {
+    children:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat consequatur quae atque nihil nemo eius dolorum at maiores quo accusantium cum, dolor odit doloribus, laudantium unde ex provident saepe corporis!',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
